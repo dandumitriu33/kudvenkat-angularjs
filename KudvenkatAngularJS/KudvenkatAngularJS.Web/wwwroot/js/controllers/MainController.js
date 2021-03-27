a8dd04a499693358204e3a923828afbcc006861d
Dan Dumitriu <dandumitriu33@gmail.com>
2021-03-27 16:15:06 +0200

    Add initial .net Web project with AngularJS basic app

---
+++ KudvenkatAngularJS/KudvenkatAngularJS.Web/wwwroot/js/controllers/MainController.js
@@ -0,0 +1,10 @@
+﻿// One way
+//var MainController = function ($scope) {
+//    $scope.message = "Angular JS practice";
+//};
+
+//app.controller('MainController', MainController);
+
+app.controller('MainController', ['$scope', function ($scope) {
+    $scope.message = "Angular JS practice";
+}]);
